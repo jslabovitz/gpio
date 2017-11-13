8f9744606d58770a4104156f3017fc4d0d5c6e62
John Labovitz <johnl@johnlabovitz.com>
2017-11-13 11:14:06 -0500

    Move Pin class into separate file.

--- lib/gpio.rb
+++ lib/gpio.rb
@@ -1,5 +1,7 @@
-require 'gpio/version'
 require 'path'
+
+require 'gpio/pin'
+require 'gpio/version'
 
 module GPIO
 
@@ -7,58 +9,6 @@ module GPIO
 
   def self.has_gpio?
     GPIOFilesystem.exist?
-  end
-
-  class Pin
-
-    attr_accessor :number
-    attr_accessor :io_mode
-    attr_accessor :edge_mode
-
-    def initialize(number, io_mode: 'r', edge_mode: 'falling')
-      @number = number
-      @io_mode = io_mode
-      @edge_mode = edge_mode    # "none", "rising", "falling", or "both"
-      @root_path = GPIOFilesystem / "gpio#{number}"
-      @edge_path = @root_path / 'edge'
-      @value_path = @root_path / 'value'
-      export
-      set_edge_mode
-      open
-    end
-
-    def export
-      (GPIOFilesystem / 'export').write(@number.to_s)
-      3.times do
-        return if @root_path.exist?
-        sleep 0.1
-      end
-      raise "No GPIO path appeared at #{@root_path}"
-    end
-
-    def unexport
-      @value_io.close
-      (GPIOFilesystem / 'unexport').write(@number.to_s)
-    end
-
-    def io_mode=(mode)
-      @io_mode = mode
-    end
-
-    def set_edge_mode
-      @edge_path.write(@edge_mode)
-    end
-
-    def open
-      @value_io = @value_path.open(@io_mode)
-    end
-
-    def read
-      value = @value_io.read
-      @value_io.seek(0, IO::SEEK_SET)
-      value
-    end
-
   end
 
 end
